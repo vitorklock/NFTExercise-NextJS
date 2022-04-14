0cc9d76ee6cfefd8ca51b3f44f37dc8e94656477
vitorklock <klockvitor@gmail.com>
2022-04-14 16:26:06 -0300

    :construction: Fazendo card section

--- src/components/SectionExibit/index.tsx
+++ src/components/SectionExibit/index.tsx
@@ -1,4 +1,5 @@
 import { Col, Row } from 'react-bootstrap';
+import { CardNft } from '../CardNft';
 import styles from './styles.module.scss';
 
 interface SectionExibit extends React.HTMLProps<HTMLDivElement> {
@@ -13,7 +14,45 @@ interface SectionExibit extends React.HTMLProps<HTMLDivElement> {
 export function SectionExibit(props: SectionExibit) {
     if (props.card) {
         return (
-            <h1>card</h1>
+            <section className={`${styles.section}`}>
+                <Row className={styles.row}>
+                    <Col xs="5" xl="6" className={`${styles.cardCol} order-first`}>
+                        {/* <Col xs="8">
+                            <img src={props.src} alt="A new NFT!" />
+                        </Col>
+                        <Col xs="4" className="d-flex align-items-end">
+                            <div className="mb-5">
+                                <span className={styles.series}>{props.series}</span>
+                                <h4>{props.name}</h4>
+                                <span className={styles.code}>{props.code}</span>
+                            </div>
+                        </Col> */}
+                        <CardNft className={`${styles.CardNft}`}
+                            src={'./images/NFTs/womanGreen.png'}
+                            width={"300px"} height={"400px"}
+                            series="gloop series" price={299}
+                            name={'Green'} code={12983}
+                            countdown={1} theme="dark"
+                        />
+                    </Col>
+                    <Col xs="7" xl="6" className="d-flex flex-column justify-content-center pe-5">
+                        <h5>Initial Release {props.releaseDate}</h5>
+                        <p>
+                            We have released four limited edition NFTs early which can be bid on via <em>OpenSea</em>.
+                            <br /><br />
+                            These will be the only four of these NFTs we ever make, so be sure not to miss out!
+                            <br /><br />
+                            50% of proceeds go to charity.
+                        </p>
+                        <a href="#" className="pb-5">
+                            Check them out
+                            <span className="ps-3">
+                                <img src="./images/icons/arrowRightPink.png" aria-hidden />
+                            </span>
+                        </a>
+                    </Col>
+                </Row>
+            </section>
         )
     } else {
         return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,130 +15,140 @@ export default function Home() {
         <title>Home | nftlanding</title>
       </Head>
       <body>
-        <section className={styles.banner}>
-          <Row className={`${styles.row} mx-3 mx-lg-5`}>
-            <Row>
-              <span>launching soon</span>
-            </Row>
-            <Row>
-              <Col xs="9" sm="10" md="10" lg="8" xxl="6">
-                <h2>
-                  An NFT like
-                  <br className="d-none d-md-block" /> no other
-                </h2>
-              </Col>
-            </Row>
-            <Row>
-              <Col
-                xs="9" sm="7" md="7" lg="5" xxl="4"
-                className="pe-0">
-                <p>
-                  {`Don't miss out on the release of our new NFT.
+        <main className={styles.main}>
+          <section className={styles.banner}>
+            <Row className={`${styles.row} mx-3 mx-lg-5`}>
+              <Row>
+                <span>launching soon</span>
+              </Row>
+              <Row>
+                <Col xs="9" sm="10" md="10" lg="8" xxl="6">
+                  <h2>
+                    An NFT like
+                    <br className="d-none d-md-block" /> no other
+                  </h2>
+                </Col>
+              </Row>
+              <Row>
+                <Col
+                  xs="9" sm="7" md="7" lg="5" xxl="4"
+                  className="pe-0">
+                  <p>
+                    {`Don't miss out on the release of our new NFT.
                   Sign up below to receive updates when we go live.`}
-                </p>
-              </Col>
+                  </p>
+                </Col>
+              </Row>
+              <SignUpBtn />
+              <div className={`${styles.imgDiv}`}>
+                <img src="/images/NFTs/robot.png" alt="New NFT!" />
+                <img src="/images/section_1/robotBg.svg" alt="New NFT!" aria-hidden />
+              </div>
             </Row>
-            <SignUpBtn />
-            <div className={`${styles.imgDiv}`}>
-              <img src="/images/NFTs/robot.png" alt="New NFT!" />
-              <img src="/images/section_1/robotBg.svg" alt="New NFT!" aria-hidden />
+          </section>
+          <section className={styles.earlyBirdsSection}>
+            <div className={`${styles.earlyBirdsRow} row px-5`}>
+              <Row>
+                <Col className="mb-4">
+                  <img src="./images/section_2/notification.svg" alt="nftlanding logo" />
+                </Col>
+              </Row>
+              <Row>
+                <Col className="mb-2">
+                  <h2>
+                    Free NFT for early birds
+                  </h2>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <span>
+                    {`Sign up today and you’ll get a free NFT when we launch.`}
+                  </span>
+                </Col>
+              </Row>
+              <div className={styles.CardsNft}>
+                <CardNft className={styles.CardNft}
+                  src={'./images/NFTs/womanBeige.png'}
+                  width={"230px"} height={"290px"}
+                  series="gloop series"
+                  price={395}
+                  name={'Beige Woman'}
+                  code={1094}
+                  countdown={2}
+                  theme="dark"
+                />
+                <CardNft className={styles.CardNft}
+                  src={'./images/NFTs/manPurple.png'}
+                  width={"230px"} height={"290px"}
+                  series="gloop series"
+                  price={299}
+                  name={'Purple Man'}
+                  code={12983}
+                  countdown={1}
+                />
+              </div>
             </div>
-          </Row>
-        </section>
-        <section className={styles.earlyBirdsSection}>
-          <div className={`${styles.earlyBirdsRow} row px-5`}>
+          </section>
+
+          <section className={styles.sponsorsSection}>
             <Row>
-              <Col className="mb-4">
-                <img src="./images/section_2/notification.svg" alt="nftlanding logo" />
-              </Col>
+              <img src="./images/section_3/logoBoomerang.png" alt="Boomerang" className="d-none d-lg-block" />
+              <img src="./images/section_3/logoBoom.png" alt="Boom" className="d-block d-lg-none" />
+              <img src="./images/section_3/logoBlimp.png" alt="Blimp" />
+              <img src="./images/section_3/logoQrco.png" alt="qrco" />
+              <img src="./images/section_3/logoOpenDoor.png" alt="OpenDoor" className="d-none d-lg-block" />
+              <img src="./images/section_3/logoDroplet.png" alt="Droplet" />
             </Row>
-            <Row>
-              <Col className="mb-2">
-                <h2>
-                  Free NFT for early birds
-                </h2>
-              </Col>
+          </section>
+
+          <section className={styles.auctionSection}>
+            <Row className={`${styles.row} mx-3 mx-md-5`}>
+              <h2>
+                LE Super Rare Auction
+              </h2>
             </Row>
-            <Row>
-              <Col>
-                <span>
-                  {`Sign up today and you’ll get a free NFT when we launch.`}
-                </span>
-              </Col>
+            <Row className={`${styles.row} mx-3 mx-md-5`}>
+              <h3>
+                We have released four limited edition NFTs early which can be bid on via <em>OpenSea</em>.
+              </h3>
             </Row>
-            <div className={styles.CardsNft}>
-              <CardNft className={styles.CardNft}
-                src={'./images/NFTs/womanBeige.png'}
-                width={"230px"} height={"290px"}
-                series="gloop series"
-                price={395}
-                name={'Beige Woman'}
-                code={1094}
-                countdown={2}
-                theme="dark"
+            <div className={`${styles.auctionRow} px-5`}>
+              <CardNft className={styles.CardNft} src={'./images/NFTs/manPurple.png'} width={"300px"}
+                height={"400px"} series="gloop series" price={299} name={'Purple Man'} code={12983}
+                countdown={1}
               />
-              <CardNft className={styles.CardNft}
-                src={'./images/NFTs/manPurple.png'}
-                width={"230px"} height={"290px"}
-                series="gloop series"
-                price={299}
-                name={'Purple Man'}
-                code={12983}
+              <CardNft className={styles.CardNft} src={'./images/NFTs/womanBeige.png'} width={"300px"}
+                height={"400px"} series="gloop series" price={299} name={'Beige'} code={12983}
+                countdown={1}
+              />
+              <CardNft className={styles.CardNft} src={'./images/NFTs/manRed.png'} width={"300px"}
+                height={"400px"} series="gloop series" price={299} name={'Red Man'} code={12983}
+                countdown={1}
+              />
+              <CardNft className={styles.CardNft} src={'./images/NFTs/womanGreen.png'} width={"300px"}
+                height={"400px"} series="gloop series" price={299} name={'Green'} code={12983}
                 countdown={1}
               />
             </div>
-          </div>
-        </section>
 
-        <section className={styles.sponsorsSection}>
-          <Row>
-            <img src="./images/section_3/logoBoomerang.png" alt="Boomerang" className="d-none d-lg-block" />
-            <img src="./images/section_3/logoBoom.png" alt="Boom" className="d-block d-lg-none" />
-            <img src="./images/section_3/logoBlimp.png" alt="Blimp" />
-            <img src="./images/section_3/logoQrco.png" alt="qrco" />
-            <img src="./images/section_3/logoOpenDoor.png" alt="OpenDoor" className="d-none d-lg-block" />
-            <img src="./images/section_3/logoDroplet.png" alt="Droplet" />
-          </Row>
-        </section>
-
-        <section className={styles.auctionSection}>
-          <Row className={`${styles.row } mx-3 mx-md-5`}>
-            <h2>
-              LE Super Rare Auction
-            </h2>
-          </Row>
-          <Row className={`${styles.row } mx-3 mx-md-5`}>
-            <h3>
-              We have released four limited edition NFTs early which can be bid on via <em>OpenSea</em>.
-            </h3>
-          </Row>
-          <div className={`${styles.auctionRow} px-5`}>
-            <CardNft className={styles.CardNft} src={'./images/NFTs/manPurple.png'} width={"300px"}
-              height={"400px"} series="gloop series" price={299} name={'Purple Man'} code={12983}
-              countdown={1}
+            <SectionExibit
+              name="Purple Man"
+              series="gloop series"
+              code={12983}
+              releaseDate="20/04"
+              src="./images/NFTs/manRedNoBG.png"
             />
-            <CardNft className={styles.CardNft} src={'./images/NFTs/womanBeige.png'} width={"300px"}
-              height={"400px"} series="gloop series" price={299} name={'Beige'} code={12983}
-              countdown={1}
+            <SectionExibit
+              name="Purple Man"
+              series="gloop series"
+              code={12983}
+              releaseDate="20/04"
+              src="./images/NFTs/manRedNoBG.png"
+              card
             />
-            <CardNft className={styles.CardNft} src={'./images/NFTs/manRed.png'} width={"300px"}
-              height={"400px"} series="gloop series" price={299} name={'Red Man'} code={12983}
-              countdown={1}
-            />
-            <CardNft className={styles.CardNft} src={'./images/NFTs/womanGreen.png'} width={"300px"}
-              height={"400px"} series="gloop series" price={299} name={'Green'} code={12983}
-              countdown={1}
-            />
-          </div>
-          
-          <SectionExibit 
-            name="Purple Man"
-            series="gloop series"
-            code={12983}
-            releaseDate="20/04"
-            src="./images/NFTs/manRedNoBG.png"
-          />
-        </section>
+          </section>
+        </main>
       </body>
     </>
   )
